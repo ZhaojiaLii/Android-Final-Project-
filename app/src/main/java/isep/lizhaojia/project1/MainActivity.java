c24dba32726ba959bb74d890c6aa9caf1bef45f9
Li Zhaojia <alpacaaa3@gmail.com>
2018-11-15 23:30:10 +0100

    accountant application

---
+++ app/src/main/java/isep/lizhaojia/project1/MainActivity.java
@@ -0,0 +1,72 @@
+package isep.lizhaojia.project1;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //declare
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //active bottom navigation listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fragment_account()).commit();
+
+        //active sidebar listener
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //close default Navigation Bar
+//        View decorView = getWindow().getDecorView();
+//        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+
+    //sidebar controller
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }        return super.onOptionsItemSelected(item);
+    }
+
+    //bottom navigation listener execution
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.navigation_bill:
+                    selectedFragment = new fragment_home();
+                    break;
+                case R.id.navigation_expenditure:
+                    selectedFragment = new fragment_expenditure();
+                    break;
+                case R.id.navigation_account:
+                    selectedFragment = new fragment_account();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+}
